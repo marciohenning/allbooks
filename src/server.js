ca01c45be200431154aedc9329638157336520e3
F6784864 Marcio Henning Feliciano De Carvalho <marciohenning@gmail.com>
2024-10-26 19:34:29 -0300

    Altera-rota

--- src/server.js
+++ src/server.js
@@ -18,6 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível)
-    console.log("Boas-vindas à API do AllBooks")através da URL http://localhost:8000")
+  console.log("Acesse essa API em http://localhost:8000")
 })
